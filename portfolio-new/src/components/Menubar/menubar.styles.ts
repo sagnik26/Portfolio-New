4b4ae8a67a8ea0542f089c65024e44bc370bf929
Sagnik Ghosh <Sagnik.Ghosh@p360.com>
2023-12-20 05:47:54 -0500

    mobile view navigation fixed

--- portfolio-new/src/components/Menubar/menubar.styles.ts
+++ portfolio-new/src/components/Menubar/menubar.styles.ts
@@ -1,8 +1,12 @@
+import useWindowSize from "@/global/useWindowSIze";
+
 const useStyles = () => {
+    const isMobile = useWindowSize("mobile");
+
     return {
-        main: 'flex flex-row justify-evenly items-center w-[380px] shadow-lg h-[40px] rounded-[40px] m-2',
+        main: `flex flex-row justify-evenly items-center ${isMobile ? 'w-[290px]' : 'w-[380px]'} shadow-lg h-[45px] rounded-[40px] m-2`,
         menuItemStyle: 'text-sm font-semibold hover:bg-green-100 hover:rounded-full p-2',
-        activeItem: 'bg-green-400 p-2 rounded-full text-white'
+        activeItem: 'bg-green-400 hover:bg-green-400 rounded-full text-white'
     }
 }
 
---
+++ portfolio-new/src/global/useWindowSIze.ts
@@ -0,0 +1,16 @@
+const screenSize: any = {
+    tablet: 768,
+    mobile: 640,
+    ipadPro: 1024,
+    laptop: 1025,
+    desktop: 1536,
+};
+
+export default function useWindowSize(device: string) {
+    if(device) {
+        return window.innerWidth <= screenSize[device]
+    }
+    else {
+        console.log("No device is specified, please provide any of [tablet || mobile || ipadPro || laptop || desktop]");
+    }
+}
