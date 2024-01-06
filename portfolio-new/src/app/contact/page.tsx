98c0a06fc58aa20e29c2410130bcf0aedacb58f4
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Air.local>
2024-01-06 16:02:09 +0530

    Added contact section in portfolio website

--- portfolio-new/src/app/contact/page.tsx
+++ portfolio-new/src/app/contact/page.tsx
@@ -1,5 +1,7 @@
 import Menubar from '@/components/Menubar'
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Contact = () => {
   return (
@@ -10,7 +12,19 @@ const Contact = () => {
       <div className='mx-5 lg:mx-10 mt-10'>
         <h1 className='font-bold text-[45px] break-word'>{`Let's`} Get in Touch: Ways to Connect with Me</h1>
         <h2 className='mt-5 text-[24px] text-gray-400'>Thank you for your interest in getting in touch with me.</h2>
-        <div className='mt-5'></div>
+        <div className='mt-10 flex flex-row justify-start'>
+          <Link href={'https://github.com/sagnik26'}>
+            <Image src={require('../../../public/assets/github.png')} alt={'github logo'} style={{ width: '40px' }} />
+          </Link>
+          
+          <Link href={'https://www.linkedin.com/in/sagnik-ghosh26/'}>
+            <Image src={require('../../../public/assets/linkedin.png')} alt={'linkedin logo'} style={{ width: '40px', marginLeft: '2rem' }} />
+          </Link>
+          
+          <Link href={'https://twitter.com/Sagnik_26'}>
+            <Image src={require('../../../public/assets/twitter.png')} alt={'github logo'} style={{ width: '40px', marginLeft: '2rem' }} />
+          </Link>
+        </div>
       </div>
     </main>
   )
